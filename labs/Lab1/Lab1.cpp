e983a542e2bf81f220a347b821a935e196e7b023
Felix Michel <98856469+Felacc@users.noreply.github.com>
2026-09-12 12:28:48 -0700

    feat(lab1): implement game logic in main()

--- labs/Lab1/Lab1.cpp
+++ labs/Lab1/Lab1.cpp
@@ -3,95 +3,108 @@
 
 int promptUserForMaximumRange()
 {
-    std::cout << "Enter the maximum range: ";
+	std::cout << "Enter the maximum range: ";
 
-    int max{};
-    std::cin >> max;
+	int max{};
+	std::cin >> max;
 
-    return max;
+	return max;
 }
 
 int pickRandomNumberInRange(int min, int max)
 {
-    return min + (rand() % (max - min + 1));
+	return min + (rand() % (max - min + 1));
 }
 
 int promptUserForGuess(int maxRange)
 {
-    std::cout << "Guess a number between 0 and " << maxRange << ": ";
+	std::cout << "Guess a number between 0 and " << maxRange << ": ";
 
-    int guess{};
-    std::cin >> guess;
+	int guess{};
+	std::cin >> guess;
 
-    return guess;
+	return guess;
 }
 
 bool promptForPlayAgain()
 {
-    std::cout << "Do you wish to play again [y/n]: ";
+	std::cout << "Do you wish to play again [y/n]: ";
 
-    char answer{};
-    std::cin >> answer;
+	char answer{};
+	std::cin >> answer;
 
-    if (answer != 'y' && answer != 'Y' && answer != 'n' && answer != 'N')
-    {
-        std::cout << "invalid input - try again\n";
-        promptForPlayAgain();
-    }
-    
-    if (answer == 'y' || answer == 'Y')
-    {
-        return true;
-    }
+	if (answer != 'y' && answer != 'Y' && answer != 'n' && answer != 'N')
+	{
+		std::cout << "invalid input - try again\n";
+		promptForPlayAgain();
+	}
 
-    if (answer == 'n' || answer == 'N')
-    {
-        return false;
-    }
-    
+	if (answer == 'y' || answer == 'Y')
+	{
+		std::cout << '\n'; // prints a new line to create space between the game rounds 
+		return true;
+	}
+
+	if (answer == 'n' || answer == 'N')
+	{
+		return false;
+	}
+
 }
 
 void playOneRound()
 {
-    std::cout << "*** Welcome to mystery number ***\n";
+	std::cout << "*** Welcome to mystery number ***\n";
 
-    int maxRange{ promptUserForMaximumRange() };
+	int maxRange{ promptUserForMaximumRange() };
 
-    int randomNumber{ pickRandomNumberInRange(0, maxRange) };
-    //std::cout << "randomNunmber is " << randomNumber << '\n'; // debug statement
+	int randomNumber{ pickRandomNumberInRange(0, maxRange) };
+	//std::cout << "randomNunmber is " << randomNumber << '\n'; // debug statement
 
-    int guess{ promptUserForGuess(maxRange) };
-    int guessCount { 1 };
-    while (guess != randomNumber)
-    {
-        std::cout << (guess > randomNumber ? "guess lower..." : "guess higher...") << '\n';
-        guessCount++;
-        guess = promptUserForGuess(maxRange);
-    }
+	int guess{ promptUserForGuess(maxRange) };
+	int guessCount{ 1 };
+	while (guess != randomNumber)
+	{
+		std::cout << (guess > randomNumber ? "guess lower..." : "guess higher...") << '\n';
+		guessCount++;
+		guess = promptUserForGuess(maxRange);
+	}
 
-    std::cout << "Correct, it took you " << guessCount << " guesses!\n";
+	std::cout << "Correct, it took you " << guessCount << " guesses.\n";
 }
 
 int main()
 {
-    // Tests
-    // --------------------------------------------------
-    // 
-    // Test promptUserForMaximumRange()
-    //std::cout << promptUserForMaximumRange();
-    // 
-    // Test pickRandomNumberInRange()
-    //for (int i = 0; i < 20; i++)
-    //{
-    //    std::cout << pickRandomNumberInRange(5, 7) << '\n';
-    //}
-    // 
-    // Test promptUserForGuess()
-    //std::cout << promptUserForGuess(5) << '\n';
-    //
-    // Test promptForPlayAgain()
-    //std::cout << promptForPlayAgain() << '\n';
-    //
-    // Test playOneRound()
-    playOneRound();
+	bool playing{ true };
+	while (playing)
+	{
+		playOneRound();
+		playing = promptForPlayAgain();
+	}
+
+	std::cout << "Thanks for playing!\n";
+
+
+
+
+		// Tests
+		// --------------------------------------------------
+		// 
+		// Test promptUserForMaximumRange()
+		//std::cout << promptUserForMaximumRange();
+		// 
+		// Test pickRandomNumberInRange()
+		//for (int i = 0; i < 20; i++)
+		//{
+		//    std::cout << pickRandomNumberInRange(5, 7) << '\n';
+		//}
+		// 
+		// Test promptUserForGuess()
+		//std::cout << promptUserForGuess(5) << '\n';
+		//
+		// Test promptForPlayAgain()
+		//std::cout << promptForPlayAgain() << '\n';
+		//
+		// Test playOneRound()
+		//playOneRound();
 }
