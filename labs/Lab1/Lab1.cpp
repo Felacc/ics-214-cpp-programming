ba78c12193b7c8b0b77f944c6af55a79ed4b800b
Felix Michel <98856469+Felacc@users.noreply.github.com>
2026-09-11 17:08:46 -0700

    feat(lab1): add promptUserForMaximumRange()

--- labs/Lab1/Lab1.cpp
+++ labs/Lab1/Lab1.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+int promptUserForMaximumRange()
+{
+    std::cout << "Enter the maximum range: ";
+
+    int max{};
+    std::cin >> max;
+
+    return max;
+
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << promptUserForMaximumRange();
 }
